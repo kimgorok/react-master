66eedf4483c63d9e9f75738f149eac8b7319eefe
kimgorok <kjyse2@naver.com>
2023-06-05 02:45:57 +0900

    ~6.6

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Coin from "./routes/Coin";
-import Coins from "./routes/Coins";
 
 interface IRouterProps {}
 
-function Router({}: IRouterProps) {
+function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/:coinId/*" element={<Coin />} />
-        <Route path="/" element={<Coins />} />
-      </Routes>
+      <Routes></Routes>
     </BrowserRouter>
   );
 }
---
+++ src/ToDoList.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+/* function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDoError("");
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Wirte a To DO" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+} */
+
+function ToDoList() {
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {};
+  console.log(formState.errors);
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", {
+            required: "비밀번호 필요",
+            minLength: {
+              value: 5,
+              message: "비번 넘 짧아",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoList;

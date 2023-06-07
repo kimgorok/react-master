6feaa2ad6a4a392d0bf76c465e2c280aed6b07ca
kimgorok <kjyse2@naver.com>
2023-06-08 01:16:23 +0900

    SVG

--- src/App.tsx
+++ src/App.tsx
@@ -24,25 +24,49 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Svg = styled.svg`
+  width: 300px;
+
+  height: 100px;
+  path {
+    stroke: white;
+    stroke-width: 2;
+  }
+`;
+
+const svg = {
+  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0" },
+  end: {
+    pathLength: 1,
+    fill: "rgba(255, 255, 255, 1",
+  },
+};
+
 function App() {
   const x = useMotionValue(0);
   const rotateZ = useTransform(x, [-255, 255], [-360, 360]);
   useMotionValueEvent(rotateZ, "change", (el) => console.log(el));
-  const gradient = useTransform(
-    x,
-    [-255, 255],
-    [
-      "linear-gradient(135deg, #f19d9d, #ff0000)",
 
-      "linear-gradient(135deg, #2f2ce9, #fffb00)",
-    ]
-  );
   const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper>
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="1em"
+        viewBox="0 0 512 512"
+      >
+        <motion.path
+          variants={svg}
+          initial="start"
+          animate="end"
+          transition={{
+            default: { duration: 5 },
+            fill: { duration: 2, delay: 3 },
+          }}
+          d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z"
+        ></motion.path>
+      </Svg>
     </Wrapper>
   );
 }
